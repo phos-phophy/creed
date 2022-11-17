166f95fae8f54b5694624bc04cf455c9553cc23f
Artem Kudisov <dark.artbeam@gmail.com>
2022-11-18 00:13:26 +0300

    Add words' spans into the document

--- src/abstract/document.py
+++ src/abstract/document.py
@@ -1,19 +1,23 @@
 from itertools import chain
-from typing import Iterable, List, Optional, Tuple
+from typing import Iterable, Optional, Tuple
 
 from .fact import AbstractFact, EntityFact, RelationFact
 from .span import Span
 
 
 class Document:
-    def __init__(self, doc_id: str, text: str, sentences: List[Span], facts: Iterable[AbstractFact],
-                 coref_chains: Optional[List[Tuple[int]]] = None):
+    def __init__(self, doc_id: str, text: str, words: Tuple[Span], sentences: Tuple[Span], facts: Iterable[AbstractFact],
+                 coref_chains: Optional[Tuple[Tuple[int]]] = None):
+
         self._doc_id = doc_id
         self._text = text
+        self._words = words
         self._sentences = sentences
         self._facts = tuple(facts)
         self._coref_chains = coref_chains
-        self._validate_sentences()
+
+        self._validate_spans(words)
+        self._validate_spans(sentences)
         self._validate_facts()
         self._validate_chains()
 
@@ -24,6 +28,10 @@ class Document:
     @property
     def text(self):
         return self._text
+
+    @property
+    def words(self):
+        return self._words
 
     @property
     def sentences(self):
@@ -37,12 +45,16 @@ class Document:
     def coref_chains(self):
         return self._coref_chains
 
-    def _validate_sentences(self):
+    @staticmethod
+    def _validate_span(text_span: Span, span: Span):
+        if span not in text_span:
+            raise ValueError(f"Span should be in the text: {span} not in {text_span}")
+
+    def _validate_spans(self, spans: Tuple[Span]):
         text_span = Span(self.text, 0, len(self.text))
 
-        for ind, sent_span in enumerate(self.sentences):
-            if sent_span not in text_span:
-                raise ValueError(f"Sentence {ind} should be in the text: {sent_span} not in {text_span}")
+        for span in spans:
+            self._validate_span(text_span, span)
 
     def _validate_chains(self):
         for idx in chain(*self.coref_chains):
@@ -52,16 +64,12 @@ class Document:
     def _validate_facts(self):
         text_span = Span(self.text, 0, len(self.text))
 
-        def validate_entity_fact(abstract_fact: EntityFact):
-            if abstract_fact.span not in text_span:
-                raise ValueError(f"Span of fact (id={abstract_fact.fact_id}) should be in text: {abstract_fact.span} not in {text_span}")
-
         for fact in self.facts:
             if isinstance(fact, EntityFact):
-                validate_entity_fact(fact)
+                self._validate_span(text_span, fact.span)
             elif isinstance(fact, RelationFact):
-                validate_entity_fact(fact.from_fact)
-                validate_entity_fact(fact.to_fact)
+                self._validate_span(text_span, fact.from_fact.span)
+                self._validate_span(text_span, fact.to_fact.span)
             else:
                 raise ValueError
 
