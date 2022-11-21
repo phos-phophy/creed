aedf1ee519318bc9f1fc604d0f2a434988711641
Artem Kudisov <dark.artbeam@gmail.com>
2022-11-21 23:42:02 +0300

    Now coreference chains are hashable

--- tests/datasets/helpers.py
+++ tests/datasets/helpers.py
@@ -8,5 +8,5 @@ def equal_docs(case: TestCase, gold_doc: Document, doc: Document):
     case.assertEqual(gold_doc.text, doc.text)
     case.assertEqual(gold_doc.words, doc.words)
     case.assertEqual(gold_doc.sentences, doc.sentences)
-    case.assertEqual(gold_doc.facts, doc.facts)
-    case.assertEqual(gold_doc.coref_chains, doc.coref_chains)
+    case.assertEqual(set(gold_doc.facts), set(doc.facts))
+    case.assertEqual(set(gold_doc.coref_chains), set(doc.coref_chains))
