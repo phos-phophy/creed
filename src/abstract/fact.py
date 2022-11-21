55f8f492e6a3698f0457bdcd083ab9f6f2b45882
Artem Kudisov <dark.artbeam@gmail.com>
2022-11-21 17:30:48 +0300

    Now facts are hashable

--- src/abstract/fact.py
+++ src/abstract/fact.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 from enum import Enum
 from typing import Tuple
 
@@ -19,6 +19,13 @@ class AbstractFact(metaclass=ABCMeta):
     def __eq__(self, other: 'AbstractFact'):
         return self.fact_id == other.fact_id and self.fact_type_id == other.fact_type_id and self.fact_type == other.fact_type
 
+    @abstractmethod
+    def __hash__(self):
+        return hash((self.fact_id, self.fact_type_id, self.fact_type))
+
+    def __repr__(self):
+        return f"{self.fact_id=}, {self.fact_type_id=}, {self.fact_type=}"
+
     @property
     def fact_id(self):
         return self._fact_id
@@ -36,13 +43,21 @@ class EntityFact(AbstractFact):
     def __init__(self, fact_id: str, fact_type_id: str, mentions: Tuple[Span]):
         super().__init__(fact_id, fact_type_id, FactType.ENTITY)
         self._mentions = mentions
+        self._validate_mentions()
 
     def __eq__(self, other: 'AbstractFact'):
         return isinstance(other, EntityFact) and super().__eq__(other) and self.mentions == other.mentions
 
+    def __hash__(self):
+        return hash((self.fact_id, self.fact_type_id, self.fact_type, self.mentions))
+
     @property
     def mentions(self):
         return self._mentions
+
+    def _validate_mentions(self):
+        if len(self.mentions) != len(set(self._validate_mentions())):
+            raise ValueError(f"EntityFact ({self}) has identical mentions!")
 
 
 class RelationFact(AbstractFact):
@@ -55,6 +70,9 @@ class RelationFact(AbstractFact):
     def __eq__(self, other: 'AbstractFact'):
         return isinstance(other, RelationFact) and super().__eq__(other) and \
             self.from_fact == other.from_fact and self.to_fact == other.to_fact
+
+    def __hash__(self):
+        return hash((self.fact_id, self.fact_type_id, self.fact_type, self.from_fact, self.to_fact))
 
     @property
     def from_fact(self):
