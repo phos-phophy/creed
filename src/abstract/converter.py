0f6bb03f790bcd11326b8af8eab2cee45995a555
Artem Kudisov <dark.artbeam@gmail.com>
2022-11-21 19:07:29 +0300

    Implement DocREDConverter

--- src/abstract/converter.py
+++ src/abstract/converter.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from pathlib import Path
-from typing import Iterable
+from typing import Iterator
 
 from .document import Document
 
@@ -8,5 +8,5 @@ from .document import Document
 class AbstractConverter(metaclass=ABCMeta):
 
     @abstractmethod
-    def convert(self, path: Path) -> Iterable[Document]:
+    def convert(self, path: Path) -> Iterator[Document]:
         pass
