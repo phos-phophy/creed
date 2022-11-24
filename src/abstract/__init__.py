694c7936cdb2f1b599abae7d8fe52b6669710f19
Artem Kudisov <dark.artbeam@gmail.com>
2022-11-24 19:12:31 +0300

    Update __init__.py

--- src/abstract/__init__.py
+++ src/abstract/__init__.py
@@ -1,12 +1,16 @@
 from .converter import AbstractConverter
 from .coreference_chain import CoreferenceChain
+from .dataset import AbstractDataset
 from .document import Document
 from .fact import AbstractFact, EntityFact, FactType, RelationFact
+from .model import AbstractModel
 from .span import Span
 
 __all__ = [
     "AbstractConverter",
+    "AbstractDataset",
     "AbstractFact",
+    "AbstractModel",
     "CoreferenceChain",
     "Document",
     "EntityFact",
