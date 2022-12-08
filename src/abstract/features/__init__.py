29731151c75f5ddf6e603ea1092ee5548928933b
Artem Kudisov <dark.artbeam@gmail.com>
2022-12-08 17:31:02 +0300

    Delete CoreferenceChain class

--- src/abstract/features/__init__.py
+++ src/abstract/features/__init__.py
@@ -1,10 +1,8 @@
-from .coreference_chain import CoreferenceChain
 from .fact import AbstractFact, EntityFact, FactType, RelationFact
 from .span import Span
 
 __all__ = [
     "AbstractFact",
-    "CoreferenceChain",
     "EntityFact",
     "FactType",
     "RelationFact",
