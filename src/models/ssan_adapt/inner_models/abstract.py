c588dfbad8f080ffa053d8534ef08d3fb707e8e7
Artem Kudisov <dark.artbeam@gmail.com>
2022-12-30 20:57:24 +0300

    Implement AbstractSSANAdaptInnerModel class

---
+++ src/models/ssan_adapt/inner_models/abstract.py
@@ -0,0 +1,46 @@
+import math
+from abc import ABCMeta
+from typing import Iterable
+
+
+from src.abstract import AbstractModel, get_tokenizer_len_attribute
+from transformers import AutoModel, AutoTokenizer
+
+
+class AbstractSSANAdaptInnerModel(AbstractModel, metaclass=ABCMeta):
+
+    def __init__(
+            self,
+            entities: Iterable[str],
+            relations: Iterable[str],
+            no_ent_ind: int,
+            pretrained_model_path: str,
+            tokenizer_path: str,
+            dist_base: int
+    ):
+        super(AbstractSSANAdaptInnerModel, self).__init__(relations)
+
+        self._entities = tuple(entities)
+        self._no_ent_ind = no_ent_ind
+
+        self._tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        self._model = AutoModel.from_pretrained(pretrained_model_path)
+
+        self._dist_base = dist_base
+
+        len_attr = get_tokenizer_len_attribute(self._tokenizer)
+
+        self._dist_ceil = math.ceil(math.log(self._tokenizer.__getattribute__(len_attr), self._dist_base)) + 1
+        self._dist_emb_dim = self._ceil * 2
+
+    @property
+    def entities(self):
+        return self._entities
+
+    @property
+    def no_ent_ind(self):
+        return self._no_ent_ind
+
+    @property
+    def dist_ceil(self):
+        return self._dist_ceil
