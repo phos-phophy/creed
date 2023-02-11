e1dbb7684922082e14a37335a24c94445b7ecf45
Artem Kudisov <dark.artbeam@gmail.com>
2023-02-12 02:09:43 +0300

    DocRED: now mentions of one entity represent only one fact (not several coreference)

--- src/loader/docred.py
+++ src/loader/docred.py
@@ -20,13 +20,6 @@ Example from DocRED consists of the following fields:
     * h: int - head (source) entity index in vertexSet
     * t: int - tail (target) entity index in vertexSet
     * evidence:  List[int] - ???
-
-WARNING: There are erroneous examples in DocRED in which the same coreference etities (from vertexSet) are used several times.
-But there are also documents with the nested entities that are ok (in the case they have identical types)
-
-E.g. in the ../docred/train_annotated.json file there are:
-* 'Nabacalis' document where second fact is embedded in the first one - it's ok
-* 'Eclipse Aviation' document where 9th set of coreference facts contains 2 identical facts (about Vern Raburn) - it is not ok
 """
 
 
@@ -50,10 +43,10 @@ class DocREDLoader(AbstractLoader):
         sentences: List[List[Span]] = self._extract_sentences(example)
         text = ' '.join(word for sentence in example['sents'] for word in sentence)
 
-        entity_facts, coref_chains = self._extract_entity_facts(example["vertexSet"], sentences)
+        entity_facts = self._extract_entity_facts(example["vertexSet"], sentences)
 
         facts: List[AbstractFact] = entity_facts
-        facts.extend(self._extract_rel_facts(example.get("labels", []), coref_chains))
+        facts.extend(self._extract_rel_facts(example.get("labels", []), entity_facts))
 
         return Document(example["title"], text, sentences, facts)
 
@@ -76,26 +69,23 @@ class DocREDLoader(AbstractLoader):
     @staticmethod
     def _extract_entity_facts(vertex_set: List[List[Dict]], sentences: List[List[Span]]):
 
-        facts: List[EntityFact] = []
-        coref_chains: List[List[EntityFact]] = []
+        def get_mention_spans(mention: dict):
+            sent_id, start, end = mention["sent_id"], mention["pos"][0], mention["pos"][1]
+            return [sentences[sent_id][span_id] for span_id in range(start, end, 1)]
 
-        def build_entity_fact(desc: dict, coref_id: int):
-            mention_spans = [sentences[desc["sent_id"]][span_id] for span_id in range(desc["pos"][0], desc["pos"][1], 1)]
-            return EntityFact("", desc["type"], str(coref_id), tuple(mention_spans))
+        def build_entity_fact(facts_desc: List[dict], coref_id: int):
+            fact_type_id = facts_desc[0]["type"]
+            mention_spans = chain.from_iterable(get_mention_spans(mention) for mention in facts_desc)
+            return EntityFact("", fact_type_id, str(coref_id), tuple(mention_spans))
 
-        for ind, coref_facts_desc in enumerate(vertex_set):
-            coref_facts = {build_entity_fact(fact_desc, ind) for fact_desc in coref_facts_desc}
-            facts.extend(coref_facts)
-            coref_chains.append(list(coref_facts))
+        return [build_entity_fact(coref_facts_desc, ind) for ind, coref_facts_desc in enumerate(vertex_set)]
 
-        return facts, coref_chains
-
-    def _extract_rel_facts(self, labels: List[Dict], coref_facts: List[List[EntityFact]]):
+    def _extract_rel_facts(self, labels: List[Dict], entity_facts: List[EntityFact]):
 
         def build_rel_fact(desc: dict):
             rel_type = self.rel_info.get(desc["r"], desc["r"])
-            from_facts = coref_facts[desc["h"]]
-            to_facts = coref_facts[desc["t"]]
-            return [RelationFact("", rel_type, from_fact, to_fact) for from_fact in from_facts for to_fact in to_facts]
+            from_fact = entity_facts[desc['h']]
+            to_fact = entity_facts[desc['t']]
+            return RelationFact("", rel_type, from_fact, to_fact)
 
-        return list(chain.from_iterable(build_rel_fact(rel_desc) for rel_desc in labels))
+        return [build_rel_fact(rel_desc) for rel_desc in labels]
