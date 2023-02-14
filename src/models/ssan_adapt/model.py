e2847aae3c48e17be89f7535d08bff83894b6933
Artem Kudisov <dark.artbeam@gmail.com>
2023-02-14 06:20:01 +0300

    SSANAdaptModel: implement evaluate function

--- src/models/ssan_adapt/model.py
+++ src/models/ssan_adapt/model.py
@@ -1,12 +1,17 @@
+import json
+from pathlib import Path
 from typing import Any, Iterable
 
+import numpy as np
 import torch
-from src.abstract import AbstractDataset, AbstractModel, Document
+from src.abstract import AbstractDataset, AbstractWrapperModel, Document
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from .inner_models import AbstractSSANAdaptInnerModel, get_inner_model
 
 
-class SSANAdaptModel(AbstractModel):
+class SSANAdaptModel(AbstractWrapperModel):
 
     def __init__(
             self,
@@ -15,20 +20,14 @@ class SSANAdaptModel(AbstractModel):
             inner_model_type: str,
             hidden_dim: int,
             dropout: float,
-            no_ent_ind: int = None,
             **kwargs
     ):
-        if no_ent_ind is None:
-            no_ent_ind = 0
-            entities = ['<NO_ENT>'] + list(entities)
-
-        self._no_ent_ind = no_ent_ind
         self._entities = tuple(entities)
 
         super(SSANAdaptModel, self).__init__(relations)
 
         self._inner_model: AbstractSSANAdaptInnerModel = get_inner_model(
-            inner_model_type=inner_model_type, entities=entities, no_ent_ind=no_ent_ind, relations=relations, **kwargs
+            inner_model_type=inner_model_type, entities=entities, relations=relations, **kwargs
         )
 
         self._dist_ceil = self._inner_model.dist_ceil
@@ -41,13 +40,11 @@ class SSANAdaptModel(AbstractModel):
         self._rel_dist_embeddings = torch.nn.Embedding(self._dist_emb_dim, self._dist_emb_dim, padding_idx=self._dist_ceil)
         self._bili = torch.nn.Bilinear(hidden_dim + self._dist_emb_dim, hidden_dim + self._dist_emb_dim, len(self.relations))
 
+        self.threshold = None
+
     @property
     def entities(self):
         return self._entities
-
-    @property
-    def no_ent_ind(self):
-        return self._no_ent_ind
 
     def forward(
             self,
@@ -86,7 +83,7 @@ class SSANAdaptModel(AbstractModel):
         if labels is not None:
             return self._compute_loss(logits, labels, labels_mask), torch.sigmoid(logits)
 
-        return logits
+        return torch.sigmoid(logits)
 
     def prepare_dataset(self, document: Iterable[Document], extract_labels=False, evaluation=False) -> AbstractDataset:
         return self._inner_model.prepare_dataset(document, extract_labels, evaluation)
@@ -113,3 +110,81 @@ class SSANAdaptModel(AbstractModel):
         mean_batch_loss = torch.mean(batch_loss)
 
         return mean_batch_loss
+
+    def evaluate(self, dataloader: DataLoader, output_path: str = None):
+
+        loss = 0.0
+        preds, ent_masks, labels_ids = [], [], []
+
+        for _, inputs in tqdm(dataloader, desc='Evaluating'):
+            self.model.eval()
+            inputs = {key: token.cuda() for key, token in inputs.items()} if torch.cuda.is_available() else inputs
+
+            with torch.no_grad():
+                batch_loss, logits = self.model(**inputs)
+                loss += batch_loss.mean().item()
+
+            preds.append(logits.detach().cpu().numpy())
+            ent_masks.append(inputs["ent_mask"].detach().cpu().numpy())
+            labels_ids.append(inputs["labels"].detach().cpu().numpy())
+
+        loss /= len(dataloader)
+        preds, ent_masks, labels_ids = np.vstack(preds), np.vstack(ent_masks), np.vstack(labels_ids)
+
+        total_labels = torch.sum(labels_ids[:, :, :, 1:]).item()
+        output_preds = []
+
+        # pred: (ent, ent, num_link)
+        # ent_mask: (ent, len)
+        # labels: (ent, ent, num_link)
+        for (pred, ent_mask, labels) in zip(preds, ent_masks, labels_ids):
+
+            # don't take into account <NO_REL> relation
+            gold_relations = [(h, t, r + 1) for (h, t, r) in zip(*np.where(labels[:, :, 1:]))]
+
+            for h in range(pred.shape[0]):
+                for t in range(pred.shape[0]):
+
+                    if h == t or np.all(ent_mask[h] == 0) or np.all(ent_mask[t] == 0):
+                        continue
+
+                    for predicate_id, logit in enumerate(pred[h][t]):
+                        if predicate_id == 0:
+                            continue
+
+                        is_right_relation = (h, t, predicate_id) in gold_relations
+                        output_preds.append((is_right_relation, logit, h, t, predicate_id))
+
+        output_preds.sort(key=lambda x: x[1], reverse=True)
+
+        pr_x = []
+        pr_y = []
+        correct = 0
+        for i, pred in enumerate(output_preds, start=1):
+            correct += pred[0]
+            pr_y.append(float(correct) / i)
+            pr_x.append(float(correct) / total_labels)
+
+        pr_x = np.asarray(pr_x, dtype='float32')
+        pr_y = np.asarray(pr_y, dtype='float32')
+        f1_arr = (2 * pr_x * pr_y / (pr_x + pr_y + 1e-20))
+        f1 = f1_arr.max()
+        f1_pos = f1_arr.argmax()
+        threshold = output_preds[f1_pos][1]
+
+        result = {
+            "loss": loss,
+            "precision": pr_y[f1_pos],
+            "recall": pr_x[f1_pos],
+            "f1": f1,
+            "threshold": threshold
+        }
+
+        with open(Path(output_path), 'r') as file:
+            json.dump(result, file)
+
+        self.threshold = threshold
+
+    def predict(self, dataloader: DataLoader, output_path: str = None):
+        if self.threshold is None:
+            raise ValueError("First calculate the threshold value using evaluate function (on dev dataset)!")
