acab9f54a03b9250026e81707f9b24d229ec7b2b
Artem Kudisov <dark.artbeam@gmail.com>
2023-02-28 22:22:03 +0300

    Implement IETypesSSANAdaptDataset

--- src/models/ssan_adapt/inner_models/ie_types/dataset.py
+++ src/models/ssan_adapt/inner_models/ie_types/dataset.py
@@ -1,85 +1,67 @@
+import itertools
 from collections import defaultdict
-from typing import Dict, Iterable, List, Tuple
+from typing import Dict, List, Tuple
 
-import numpy as np
 import torch
-from src.abstract import AbstractDataset, Document, EntityFact, FactClass, NO_ENT_IND, NO_REL_IND, PreparedDocument, RelationFact, Span, \
-    get_tokenizer_len_attribute
+from src.abstract import Document, EntityFact, FactClass, NO_ENT_IND, Span
 
 from ..base.dataset import BaseSSANAdaptDataset
 
 
 class IETypesSSANAdaptDataset(BaseSSANAdaptDataset):
+    ENT_IND = NO_ENT_IND + 1
 
-    def _prepare_document(self, document: Document):
-        """
-        1) input_ids: LongTensor (len,)
-        2) ner_ids: LongTensor  (len,)
-        3) dist_ids: LongTensor (max_ent, max_ent)
-        4) ent_mask: BoolTensor (max_ent, len)
-        5) attention_mask: BoolTensor (len,)
-        6) struct_matrix: BoolTensor (5, len, len)
-        7) labels: BoolTensor (max_ent, max_ent, num_link)
-        8) labels_mask: BoolTensor (max_ent, max_ent)
-        """
+    def _get_ent_tokens(self, document: Document):
 
-        if len(document.sentences) == 0:
-            return
+        spans = tuple(itertools.chain.from_iterable(document.sentences))
+        spans_to_ind = {span: ind for ind, span in enumerate(spans)}
 
-        # 1st stage: tokenize text, get input_ids and extract entity facts
-        # token_to_sentence - map token_ind to the corresponding sentence_ind
-        # span_to_token_ind - map span to the corresponding token_ind's
-        input_ids, token_to_sentence_ind, span_to_token_ind = self._tokenize(document)
-        ner_facts = self._extract_ner_facts(document, span_to_token_ind)
+        start, end = [()] * len(spans), [()] * len(spans)
 
-        if len(ner_facts) == 0:
-            return
+        ner_facts: Tuple[EntityFact, ...] = tuple(filter(lambda fact: fact.fact_class is FactClass.ENTITY, document.facts))[:self.max_ent]
 
-        # 2nd stage: get ner_ids and ent_mask
-        # token_to_coreference_id - map token_ind to the coreference_id of the corresponding fact
-        input_ids, token_to_sentence_ind, ent_mask, token_to_coreference_id = self._populate_ner_information(
-            input_ids, token_to_sentence_ind, span_to_token_ind, ner_facts
-        )
+        for ner_fact in ner_facts:
+            for span in ner_fact.mentions:
+                span_ind = spans_to_ind[span]
 
-        # 3rd stage: get dist_ids and struct_matrix
-        dist_ids = self._extract_dist_ids(ent_mask)
-        struct_matrix = self._extract_struct_matrix(token_to_sentence_ind, token_to_coreference_id)
+                if span_ind > 0:
+                    if end[span_ind - 1] != (ner_fact.coreference_id, ner_fact.type_id):
+                        start[span_ind] = (ner_fact.coreference_id, ner_fact.type_id)
+                    else:
+                        end[span_ind - 1] = ()
+                else:
+                    start[span_ind] = (ner_fact.coreference_id, ner_fact.type_id)
 
-        # 4th stage: normalize ent_mask
-        tmp = ent_mask.sum(dim=-1)
-        tmp = tmp + (tmp == 0)
-        ent_mask = ent_mask / tmp.unsqueeze(1)
+                if span_ind - 1 < len(spans):
+                    if start[span_ind + 1] != (ner_fact.coreference_id, ner_fact.type_id):
+                        end[span_ind] = (ner_fact.coreference_id, ner_fact.type_id)
+                    else:
+                        start[span_ind + 1] = ()
+                else:
+                    end[span_ind] = (ner_fact.coreference_id, ner_fact.type_id)
 
-        features = {
-            "input_ids": input_ids,
-            "dist_ids": dist_ids,
-            "ent_mask": ent_mask,
-            "attention_mask": torch.ones(input_ids.shape[0]).bool(),
-            "struct_matrix": struct_matrix,
-        }
+        start_ent_tokens = [f' <{s[1]}> ' if s else '' for s in start]
+        end_ent_tokens = [f' </{e[1]}> ' if e else '' for e in end]
 
-        labels = None
-        if self.extract_labels:  # 5th stage: extract labels and labels_mask
-            labels_tensors, labels_mask = self._extract_labels_and_mask(ner_facts, self._extract_link_facts(document))
-            labels = {"labels": labels_tensors, "labels_mask": labels_mask}
-
-        prepared_document = PreparedDocument(features=features, labels=labels)
-
-        self._documents.append(prepared_document)
+        return start_ent_tokens, end_ent_tokens
 
     def _tokenize(self, document: Document):
 
         input_ids, token_to_sentence_ind, token_to_span = [], [], []
+        start_ent_tokens, end_ent_tokens = self._get_ent_tokens(document)
 
+        cur_ind = 0
         for ind, sentence in enumerate(document.sentences):
             for span in sentence:
-                tokens = self.tokenizer.encode(document.get_word(span))[1:-1]  # crop tokens of the beginning and the end
+                word = start_ent_tokens[cur_ind] + document.get_word(span) + end_ent_tokens[cur_ind]
+                tokens = self.tokenizer.encode(word)[1:-1]  # crop tokens of the beginning and the end
 
                 tokens = tokens if len(tokens) else [self.tokenizer.unk_token_id]
 
                 input_ids.extend(tokens)
                 token_to_sentence_ind += [ind] * len(tokens)
                 token_to_span += [span] * len(tokens)
+                cur_ind += 1
 
         input_ids = input_ids[:self.max_len - 2]
         token_to_sentence_ind = token_to_sentence_ind[:self.max_len - 2]
@@ -94,26 +76,6 @@ class IETypesSSANAdaptDataset(BaseSSANAdaptDataset):
 
         return torch.tensor(input_ids, dtype=torch.long), token_to_sentence_ind, span_to_token_ind
 
-    def _populate_ner_information(self, input_ids: torch.Tensor, token_to_sentence_ind: List, span_to_token_ind: Dict[Span, List[int]], ner_facts: Tuple[EntityFact, ...]):
-        max_ent = self.max_ent if self.max_ent else len(ner_facts)
-
-        ent_mask = [[0] * 1 for _ in range(max_ent)]
-        token_to_coreference_id = [type(self).USUAL_TOKEN] * seq_len
-
-        for ind, fact in enumerate(ner_facts):
-            ind_of_type_id = self._ent_to_ind[fact.type_id]
-
-            for span in fact.mentions:
-                for token_ind in span_to_token_ind.get(span, []):
-                    ner_ids[token_ind] = ind_of_type_id
-                    ent_mask[ind][token_ind] = True
-                    token_to_coreference_id[token_ind] = fact.coreference_id
-
-        ent_mask = torch.tensor(ent_mask, dtype=torch.bool)
-
-        return input_ids, token_to_sentence_ind, ent_mask, token_to_coreference_id
-
-
     def _extract_ner_types(self, ner_facts: Tuple[EntityFact, ...], span_to_token_ind: Dict[Span, List[int]], seq_len: int):
 
         max_ent = self.max_ent if self.max_ent else len(ner_facts)
@@ -123,7 +85,7 @@ class IETypesSSANAdaptDataset(BaseSSANAdaptDataset):
         token_to_coreference_id = [type(self).USUAL_TOKEN] * seq_len
 
         for ind, fact in enumerate(ner_facts):
-            ind_of_type_id = self._ent_to_ind[fact.type_id]
+            ind_of_type_id = type(self).ENT_IND
 
             for span in fact.mentions:
                 for token_ind in span_to_token_ind.get(span, []):
--- src/models/ssan_adapt/inner_models/ie_types/model.py
+++ src/models/ssan_adapt/inner_models/ie_types/model.py
@@ -1,4 +1,4 @@
-from typing import Any, Iterable
+from typing import Iterable
 
 from src.abstract import Document
 from src.models.ssan_adapt.inner_models.base import BaseSSANAdaptInnerModel
@@ -7,30 +7,10 @@ from .dataset import IETypesSSANAdaptDataset
 
 
 class IETypesSSANAdaptInnerModel(BaseSSANAdaptInnerModel):
+    def __init__(self, **kwargs):
+        stub_entities = ('NO_ENT', 'ENT')
+        super(BaseSSANAdaptInnerModel, self).__init__(entities=stub_entities, **kwargs)
+
     def prepare_dataset(self, documents: Iterable[Document], extract_labels=False, evaluation=False) -> IETypesSSANAdaptDataset:
-        stub_entities = ()
-        return IETypesSSANAdaptDataset(documents, self._tokenizer, extract_labels, evaluation, stub_entities, self.relations,
+        return IETypesSSANAdaptDataset(documents, self._tokenizer, extract_labels, evaluation, self.stub_entities, self.relations,
                                        self._dist_base, self._dist_ceil)
-
-    def forward(
-            self,
-            input_ids=None,  # (bs, len)
-            attention_mask=None,  # (bs, len)
-            struct_matrix=None,  # (bs, 5, len, len),
-            **kwargs
-    ) -> Any:
-
-        struct_matrix = struct_matrix.transpose(0, 1)[:, :, None, :, :]  # (5, bs, 1, len, len)
-
-        for layer in self._model.encoder.layer:
-            layer.attention.self.struct_matrix = struct_matrix
-
-        output = self._model(input_ids=input_ids, attention_mask=attention_mask, **kwargs)
-
-        for layer in self._model.encoder.layer:
-            del layer.attention.self.struct_matrix
-
-        return output
-
-    def _redefine_model_structure(self):
-        self._redefine_attention()
