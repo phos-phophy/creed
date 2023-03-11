c0dd02d5d517fea122716985d6e652abf8412e45
Artem Kudisov <dark.artbeam@gmail.com>
2023-03-11 12:26:00 +0300

    Replace NamedTuple with dataclass

--- src/manager/config.py
+++ src/manager/config.py
@@ -1,17 +1,18 @@
-from typing import NamedTuple
+from dataclasses import dataclass, field
 
 from src.abstract import DiversifierConfig
 
 from .manager import InitConfig, TrainingConfig
 
 
-class MainConfig(NamedTuple):
+@dataclass
+class MainConfig:
     loader_config: dict
     init_config: dict  # InitConfig
 
     seed: int = 42
     save_path: str = None
-    training_config: dict = {}  # TrainingConfig
+    training_config: dict = field(default_factory=dict)  # TrainingConfig
 
     train_dataset_path: str = None
     dev_dataset_path: str = None
@@ -23,13 +24,13 @@ class MainConfig(NamedTuple):
     output_test_path: str = None
     output_pred_path: str = None
 
-    train_diversifier: dict = {}  # DiversifierConfig
-    dev_diversifier: dict = {}  # DiversifierConfig
-    eval_diversifier: dict = {}  # DiversifierConfig
-    test_diversifier: dict = {}  # DiversifierConfig
-    pred_diversifier: dict = {}  # DiversifierConfig
+    train_diversifier: dict = field(default_factory=dict)  # DiversifierConfig
+    dev_diversifier: dict = field(default_factory=dict)  # DiversifierConfig
+    eval_diversifier: dict = field(default_factory=dict)  # DiversifierConfig
+    test_diversifier: dict = field(default_factory=dict)  # DiversifierConfig
+    pred_diversifier: dict = field(default_factory=dict)  # DiversifierConfig
 
-    def init(self):
+    def __post_init__(self):
         self.init_config: InitConfig = InitConfig(**self.init_config)
         self.training_config: TrainingConfig = TrainingConfig(**self.training_config)
 
@@ -38,5 +39,3 @@ class MainConfig(NamedTuple):
         self.eval_diversifier: DiversifierConfig = DiversifierConfig(**self.eval_diversifier)
         self.test_diversifier: DiversifierConfig = DiversifierConfig(**self.test_diversifier)
         self.pred_diversifier: DiversifierConfig = DiversifierConfig(**self.pred_diversifier)
-
-        return self
