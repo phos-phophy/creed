0edcee1c1fe8fdda571e84359b60d3702be0c31b
Artem Kudisov <dark.artbeam@gmail.com>
2023-04-12 06:22:07 +0300

    Set the seed value as required script argument

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from src.manager import ManagerConfig, ModelManager
 
 
 def load_manager():
-    """ Parse arguments and load main config """
+    """ Parses arguments and loads main config """
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config_path", type=str, metavar="<path to config>", required=True)
+    parser.add_argument("-s", "--seed", type=int, metavar="<seed>", required=True)
 
     arguments = parser.parse_args()
     config_path = arguments.config_path
+    seed = arguments.seed
 
     with Path(config_path).open('r') as file:
-        config: ManagerConfig = ManagerConfig.from_dict(json.load(file))
+        config_dict = json.load(file)
+        config_dict['seed'] = seed
+        config: ManagerConfig = ManagerConfig.from_dict(config_dict)
 
     return ModelManager(config)
 
--- scripts/main.sh
+++ scripts/main.sh
@@ -2,12 +2,13 @@
 
 cuda=
 
-while getopts v:c: flag
+while getopts v:c:s: flag
 do
   case "${flag}" in
     v) cuda=${OPTARG} ;;
     c) config=${OPTARG} ;;
-    *) echo "usage: $0 [-v] [-c]" >&2
+    s) seed=${OPTARG} ;;
+    *) echo "usage: $0 [-v] [-c] [-s]" >&2
        exit 1 ;;
   esac
 done
@@ -16,4 +17,4 @@ run_path="main.py"
 
 export CUDA_VISIBLE_DEVICES=$cuda
 
-python3 $run_path -c "$config"
+python3 $run_path -c "$config" -s "$seed"
