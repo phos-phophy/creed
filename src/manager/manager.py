b4b9d3111dc14da4770cf8d54ce1b786e307f0e4
Artem Kudisov <dark.artbeam@gmail.com>
2023-04-14 05:20:58 +0300

    Fix cache related bug

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -44,12 +44,13 @@ class ModelManager:
 
         train_diversifier = self.config.train_diversifier
         dev_diversifier = self.config.dev_diversifier
+        cache_dir = Path(self.config.cache_dir) if self.config.cache_dir else None
 
         train_dataset = self.model.prepare_dataset(
-            train_documents, train_diversifier, train_desc, True, False, Path(self.config.cache_dir), self.config.train_dataset_path
+            train_documents, train_diversifier, train_desc, True, False, cache_dir, self.config.train_dataset_path
         )
         dev_dataset = self.model.prepare_dataset(
-            dev_documents, dev_diversifier, dev_desc, True, True, Path(self.config.cache_dir), self.config.dev_dataset_path
+            dev_documents, dev_diversifier, dev_desc, True, True, cache_dir, self.config.dev_dataset_path
         ) if dev_documents else None
 
         dev_dataset = dev_dataset.prepare_documents() if dev_dataset else None
@@ -84,9 +85,10 @@ class ModelManager:
 
         diversifier = self.config.eval_diversifier
         batch_size = self.config.training_config.training_arguments.get("per_device_eval_batch_size", 5)
+        cache_dir = Path(self.config.cache_dir) if self.config.cache_dir else None
 
         dataset = self.model.prepare_dataset(
-            documents, diversifier, 'Prepare eval dataset', True, True, Path(self.config.cache_dir), self.config.eval_dataset_path
+            documents, diversifier, 'Prepare eval dataset', True, True, cache_dir, self.config.eval_dataset_path
         ).prepare_documents()
 
         dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
@@ -107,9 +109,10 @@ class ModelManager:
 
         diversifier = self.config.test_diversifier
         batch_size = self.config.training_config.training_arguments.get("per_device_eval_batch_size", 5)
+        cache_dir = Path(self.config.cache_dir) if self.config.cache_dir else None
 
         dataset = self.model.prepare_dataset(
-            documents, diversifier, 'Prepare test dataset', True, True, Path(self.config.cache_dir), self.config.test_dataset_path
+            documents, diversifier, 'Prepare test dataset', True, True, cache_dir, self.config.test_dataset_path
         ).prepare_documents()
 
         dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
@@ -128,9 +131,10 @@ class ModelManager:
 
         diversifier = self.config.pred_diversifier
         batch_size = self.config.training_config.training_arguments.get("per_device_eval_batch_size", 5)
+        cache_dir = Path(self.config.cache_dir) if self.config.cache_dir else None
 
         dataset = self.model.prepare_dataset(
-            documents, diversifier, 'Prepare pred dataset', False, True, Path(self.config.cache_dir), self.config.pred_dataset_path
+            documents, diversifier, 'Prepare pred dataset', False, True, cache_dir, self.config.pred_dataset_path
         ).prepare_documents()
 
         dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
