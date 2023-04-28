96a17076020637516a82789ac09225a7affcfba3
Artem Kudisov <dark.artbeam@gmail.com>
2023-04-28 22:03:08 +0300

    Add docstrings for AbstractLoader

--- src/abstract/loader/loader.py
+++ src/abstract/loader/loader.py
@@ -6,6 +6,7 @@ from src.abstract.example.document import Document
 
 
 class AbstractLoader(metaclass=ABCMeta):
+    """ An abstract base class that loads datasets and converts them into a unified document format """
 
     @abstractmethod
     def load(self, path: Path) -> Iterator[Document]:
